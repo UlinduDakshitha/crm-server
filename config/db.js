e51c89854624337d05b55065f3464c92ac8c554e
Bandara W.M.U.D. <166277247+UlinduDakshitha@users.noreply.github.com>
2026-05-06 22:00:57 +0530

    correct issue

--- config/db.js
+++ config/db.js
@@ -1,11 +1,20 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 const connectDB = async () => {
+  const uri = process.env.MONGO_URI;
+  if (!uri) {
+    console.error(
+      "MONGO_URI is not set. Create a .env file with MONGO_URI=your_connection_string",
+    );
+    process.exit(1);
+  }
+
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(uri);
     console.log("MongoDB Connected");
   } catch (err) {
-    console.error(err);
+    console.error("Failed to connect to MongoDB:", err.message || err);
     process.exit(1);
   }
 };
