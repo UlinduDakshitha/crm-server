21995fbe18a005d3f6f146f5a710f444dd45c878
Bandara W.M.U.D. <166277247+UlinduDakshitha@users.noreply.github.com>
2026-05-06 20:38:46 +0530

    error handling middleware create

--- server.js
+++ server.js
@@ -19,3 +19,7 @@ app.use("/api/dashboard", require("./routes/dashboardRoutes"));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+const errorHandler = require("./middleware/errorMiddleware");
+
+app.use(errorHandler);
