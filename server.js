c045bbc06272b317897f0ae69244ccc36b2713b2
Bandara W.M.U.D. <166277247+UlinduDakshitha@users.noreply.github.com>
2026-05-06 22:51:13 +0530

    update

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
+require('dotenv').config();
+
 const express = require("express");
 const cors = require("cors");
-const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 
-dotenv.config();
 connectDB();
 
 const app = express();
@@ -21,5 +21,4 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 const errorHandler = require("./middleware/errorMiddleware");
-
 app.use(errorHandler);
